271f72092560755026ddac9e2da731392602d273
Alfonso <alfof87@gmail.com>
2020-09-24 17:52:59 +0200

    laravel-base-crud

--- laravel-base-crud/resources/views/layouts/partials/main.blade.php
+++ laravel-base-crud/resources/views/layouts/partials/main.blade.php
@@ -1,7 +1,7 @@
 <div class="" style="width:200px;height:100vh;background-color:orange;">
   @foreach ($paganti as $pagante)
     @php
-      echo $pagante["name"] . $pagante["lastname"] . "<br>";
+      echo $pagante["name"] . " " . $pagante["lastname"] . "<br>";
     @endphp
   @endforeach
 </div>
